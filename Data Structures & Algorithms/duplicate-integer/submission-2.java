4fb2de3362ef1ac952e9de0316b58e9dd416381f
Ghost_V <ghost14731@gmail.com>
2026-07-15 16:39:30 +0530

    Add: duplicate-integer - submission-2

---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        int target=0; 
+      for(int i=0;i<nums.length;i++){
+        target=nums[i];
+        for(int j=i+1;j<nums.length;j++){
+            if(target==nums[j]){
+                return true;   }
+    }
+     }
+     return false;
+}}
